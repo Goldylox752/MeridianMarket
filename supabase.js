4ae7a71223d82dd480ead36e211ce3411ca076db
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-05-19 14:07:43 -0600

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,10 @@
+// supabase.js
+
+const SUPABASE_URL = "YOUR_SUPABASE_URL";
+const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+
+// create single reusable client
+export const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
